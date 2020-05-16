2e685ac211e81f215756583a4207303f2a33500a
Julian Secomb <hoodooprojects@gmail.com>
2020-05-16 12:17:49 -0700

    delete readme app, wipe db password

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
         port: 3306,
         host: "localhost",
         user: "root",
-        password: "rootroot",
+        password: null,
         database: "burgers_db"
     })
 };
